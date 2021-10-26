dbf16394f6a29b965d04ec759afaa20b62ccc6de
maturu <maturu.410@gmail.com>
2021-10-26 22:07:42 +0900

    fix(router): simple react routing

--- web_frontend/src/App.tsx
+++ web_frontend/src/App.tsx
@@ -1,43 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.scss';
+import { HomeScreen } from './pages/Home';
+import { WorkspaceScreen } from './pages/Workspace';
+import { SignInScreen } from './pages/SignIn';
+import { SignUpScreen } from './pages/SignUp';
+import { CreateMapScreen } from './pages/CreateMapScreen';
 
-type Routing = {
-  path: string;
-  component: React.ReactNode;
-};
-
-export const App = () => {
-  const paths: Array<Routing> = [
-    {
-      path: '/',
-      component: <h1>HOME</h1>,
-    },
-    {
-      path: '/signin',
-      component: <h1>Sign In</h1>,
-    },
-    {
-      path: '/signup',
-      component: <h1>Sign Up</h1>,
-    },
-    {
-      path: '/create-map-server',
-      component: <h1>Create Map Server</h1>,
-    },
-  ];
-
-  return (
-    <div className="App">
-      <Router>
-        <Switch>
-          {paths.map((elm) => (
-            <Route path={elm.path} key={elm.path} exact>
-              {elm.component}
-            </Route>
-          ))}
-        </Switch>
-      </Router>
-    </div>
-  );
-};
+export const App = () => (
+  <div className="App">
+    <Router>
+      <Switch>
+        <Route exact path="/signin" component={SignInScreen} />
+        <Route exact path="/signup" component={SignUpScreen} />
+        <Route exact path="/" component={HomeScreen} />
+        <Route exact path="/workspace" component={WorkspaceScreen} />
+        <Route exact path="/createmap" component={CreateMapScreen} />
+      </Switch>
+    </Router>
+  </div>
+);
