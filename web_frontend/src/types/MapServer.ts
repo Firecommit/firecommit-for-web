23b522711ece61e9618cb5b69b3134d711d7f88c
Syaz0326 <tadakazu326@gmail.com>
2021-12-12 14:58:34 +0900

    feat(DB scheme): Add workspace element inAudit

--- web_frontend/src/types/MapServer.ts
+++ web_frontend/src/types/MapServer.ts
@@ -1,6 +1,7 @@
 export type MapServer = {
   id: string;
   name: string;
+  inAudit: boolean; // True when creating map
   iconURL?: string;
   maps: { [key: string]: string }; // Layer{n}: URL
   adminUserId: string;
